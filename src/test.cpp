3d5a637ce5b230e86b95c83acdc395923f91eb0b
SuraRengoku <jiahao.liu@rwth-aachen.de>
2024-05-21 01:21:28 +0200

    fix

--- src/test.cpp
+++ src/test.cpp
@@ -7,9 +7,7 @@ int main(){
     // int mid=l+((r-l)>>1);
     // cout<<mid<<"\n";
     // cout<<"可以输出中文"<<endl;
-
     vector<int> nums={6,6,7,8,8,9};
-    cout<<lower_bound1(nums, 8)<<endl;
-
+    int index=lower_bound1(nums, 8);
     return 0;
 }
