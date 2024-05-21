4508e0f8128c2aa8ec41ec749e1661a0f78e8d10
SuraRengoku <jiahao.liu@rwth-aachen.de>
2024-05-21 17:50:39 +0200

    BinarySearchTest

--- src/test.cpp
+++ src/test.cpp
@@ -7,8 +7,10 @@ int main(){
     // int mid=l+((r-l)>>1);
     // cout<<mid<<"\n";
     // cout<<"可以输出中文"<<endl;
-    vector<int> nums={4,5,5,5,6,7,8,8,9,10,11,12};
-    int index=lower_bound1(nums, 8);
-    printf("%d",index);
+    vector<int> nums={4,5,5,5,5,6,8,8,9,10,11,12};
+    int index=lower_bound1(nums, 13);
+    // printf("%d",index);
+    vector<int> nums2={1};
+    cout<<lower_bound1(nums2,2);
     return 0;
 }
